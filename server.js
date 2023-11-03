2955eb37c0908b32fa325766e709b7120c13ddf9
Gatsbeau <jabriel0ne@gmail.com>
2023-11-02 22:27:10 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const PORT = 3001;
+const app = express();
+const dbData = require('./db/db.json')
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "notes.html")))
+
+app.get('/api/notes', (req, res)=> res.json(dbData))
+
+app.post('/api/notes', )
+
+app.listen(PORT, () =>
+  console.log(`App currently listening at http://localhost:${PORT}`)
+);
+
